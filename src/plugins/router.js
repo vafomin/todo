84491b6330ce107e07b1a14e27c21ad22de6e0cb
Viktor <enotcode@gmail.com>
2020-04-24 13:32:12 +0300

    fix: deleted the routing templates

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,9 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "../views/Home";
-import Page from "../views/Page";
-import NotFound from "../views/NotFound";
-import store from '../store';
 
 Vue.use(Router);
 
@@ -13,30 +9,14 @@ const router = new Router({
         {
             path: "/",
             name: "home",
-            component: Home,
-        },
-        {
-            path: "/page",
-            name: "page",
-            component: Page,
-            meta: {requiresAuth: true}
+            component: () => import("../views/Home"),
         },
         {
             path: "*",
             name: "notFound",
-            component: NotFound
+            component: () => import("../views/NotFound"),
         }
     ]
-});
-
-router.beforeEach((to, from, next) => {
-    let requiresAuth = to.matched.some(record => record.meta.requiresAuth);
-
-    if (requiresAuth && !store.getters.isAuth) {
-        next('/');
-    } else {
-        next();
-    }
 });
 
 export default router;
