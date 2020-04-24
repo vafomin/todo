b46b12e1dedd36fec7b49cc629f20237fac64f06
Viktor <enotcode@gmail.com>
2020-04-24 12:37:18 +0300

    feature: added task card

---
+++ src/components/TaskCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-card
+            class="mx-auto"
+            width="40em"
+    >
+        <v-card-text>
+            <p class="headline">{{ task }}</p>
+        </v-card-text>
+        <v-card-actions>
+            <v-btn color="success" outlined>
+                <v-icon>mdi-check</v-icon>
+                {{ $t("buttons.done") }}
+            </v-btn>
+            <v-btn color="error" outlined>
+                <v-icon>mdi-delete</v-icon>
+                {{ $t("buttons.delete") }}
+            </v-btn>
+
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: "TaskCard",
+        props: {
+            task: String
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
