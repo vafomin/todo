22840c90b62dfb01c591dc67e9ebc709083e11d0
Viktor <enotcode@gmail.com>
2020-04-24 14:03:01 +0300

    fix: fixed the text field size

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,34 @@
 <template>
     <div class="v-container pa-4">
-        <v-layout class="text-xs-center" column="column" justify-center="justify-center" align-center="align-center">
-            <v-flex xs12="xs12" md10="md10">
-                <v-tabs v-model="tab" grow>
-                    <v-tab>{{ $t("tabs.tasks") }}</v-tab>
-                    <v-tab :disabled="disabled">{{ $t("tabs.done") }}</v-tab>
-                </v-tabs>
+        <v-layout column="column" justify-center="justify-center" align-center="align-center">
+            <v-tabs v-model="tab" grow>
+                <v-tab>{{ $t("tabs.tasks") }}</v-tab>
+                <v-tab :disabled="disabled">{{ $t("tabs.done") }}</v-tab>
+            </v-tabs>
 
-                <v-tabs-items v-model="tab">
-                    <v-tab-item>
-                        <div class="tab-item-wrapper">
-                            <br>
-                            <v-text-field
-                                    v-model="task"
-                                    :label="$t('enterTask')"
-                                    solo
-                                    style="margin: 15px;"
-                            >
-                                <template slot="append">
-                                    <v-btn outlined @click="addTask">
-                                        {{ $t("buttons.add") }}
-                                    </v-btn>
-                                </template>
-                            </v-text-field>
-                            <TaskCard v-for="task in taskList" :key="task" :task="task"/>
-                        </div>
-                    </v-tab-item>
-                    <v-tab-item class="tab-item-wrapper">
-                        <DoneTaskCard v-for="task in doneList" :key="task" :task="task"/>
-                    </v-tab-item>
+            <v-tabs-items v-model="tab">
+                <v-tab-item>
+                    <div class="tab-item-wrapper">
+                        <v-text-field
+                                v-model="task"
+                                :label="$t('enterTask')"
+                                solo
+                                style="margin: 15px; width: 70vw;"
+                        >
+                            <template slot="append">
+                                <v-btn outlined @click="addTask">
+                                    {{ $t("buttons.add") }}
+                                </v-btn>
+                            </template>
+                        </v-text-field>
+                        <TaskCard v-for="task in taskList" :key="task" :task="task"/>
+                    </div>
+                </v-tab-item>
+                <v-tab-item class="tab-item-wrapper">
+                    <DoneTaskCard v-for="task in doneList" :key="task" :task="task"/>
+                </v-tab-item>
 
-                </v-tabs-items>
-            </v-flex>
+            </v-tabs-items>
         </v-layout>
     </div>
 </template>
@@ -73,8 +70,7 @@
 
 <style>
     .tab-item-wrapper {
-        width: 50em;
-        height: calc(100vh - 48px);
+        height: calc(90vh - 48px);
         overflow-y: auto;
     }
 </style>
