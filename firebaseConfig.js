5b42e27bd6ad39f8afed847b872451238215d1d8
Viktor <enotcode@gmail.com>
2020-05-06 20:04:50 +0300

    chore: firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,31 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: process.env.VUE_APP_API_KEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_DATABASE_URL,
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_APP_ID,
+    measurementId: process.env.VUE_APP_MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+const tasksCollection = db.collection('tasks');
+const doneCollection = db.collection('done');
+
+const auth = firebase.auth();
+
+export {
+    firebase,
+    db,
+    tasksCollection,
+    doneCollection,
+    auth
+}
