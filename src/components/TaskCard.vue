588eb5122a67a015f40fc2f3a44ae6dde70c8b4e
Viktor <enotcode@gmail.com>
2020-05-09 00:11:01 +0300

    fix: fixed double click done

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -5,11 +5,11 @@
             <small>{{ $t("created") }} {{ created | dateTransform }}</small>
         </v-card-text>
         <v-card-actions>
-            <v-btn color="success" outlined @click="done">
+            <v-btn color="success" outlined @click="done" :disabled="disabled">
                 <v-icon>mdi-check</v-icon>
                 {{ $t("buttons.done") }}
             </v-btn>
-            <v-btn color="error" outlined @click="del">
+            <v-btn color="error" outlined @click="del" :disabled="disabled">
                 <v-icon>mdi-delete</v-icon>
                 {{ $t("buttons.delete") }}
             </v-btn>
@@ -28,6 +28,11 @@
             task: String,
             created: undefined
         },
+        data() {
+            return {
+                disabled: false
+            }
+        },
         methods: {
             ...mapActions(["deleteTask", "doneTask"]),
             del() {
@@ -35,9 +40,10 @@
                 this.deleteTask({id});
             },
             done() {
+                this.disabled = true;
                 let id = this.id;
                 let task = this.task;
-                this.doneTask({id, task})
+                this.doneTask({id, task}).then(() => this.disabled = false);
             },
         }
     }
