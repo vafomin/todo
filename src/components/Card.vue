c85bb3d1d403a9a6aea2761b866750a32f771f97
Viktor <enotcode@gmail.com>
2020-05-08 04:51:41 +0300

    feature: added card for board

---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-card class="mx-4 mx-sm-auto my-4" width="75vw">
+        <v-card-text>
+            <p class="handle headline">{{ task }}</p>
+            <small>{{ $t("created") }} {{ created | dateTransform }}</small>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: "Card",
+        props: {
+            id: String,
+            task: String,
+            created: Object
+        }
+    }
+</script>
